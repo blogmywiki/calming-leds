744062f8650d8e1936fba69e43f8a70aca0b24ca
Giles Booth <giles.booth@gmail.com>
2022-10-05 10:28:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.forever(function () {
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(500)
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(2000)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
